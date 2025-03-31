6b01ce9855549f266acaa1f5ecf2852468461067
Oleksandr Sliusarchuk <o.sliusarchuk@heavensoft.com.ua>
2025-03-31 19:26:37 +0300

    page about service

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -13,4 +13,32 @@ function domLoad() {
             header.classList.remove("scrolled");
         }
     });
+
+    // ---
+
+    const buttons = document.querySelectorAll(".js-item-ser");
+    const cards = document.querySelectorAll(".js-card-s");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            buttons.forEach((but) => {
+                but.classList.remove("active");
+            });
+
+            button.classList.add('active')
+
+            const target = button.getAttribute("data-item");
+
+            cards.forEach(card => {
+                card.classList.remove("active");
+            });
+
+
+            const activeCard = document.querySelector(`.card-s[data-item="${target}"]`);
+
+            if (activeCard) {
+                activeCard.classList.add("active");
+            }
+        });
+    });
 }
